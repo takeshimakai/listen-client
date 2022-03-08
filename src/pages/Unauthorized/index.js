bc1741746fc25bcf60f7dddb66a592c327b22bdc
Kai Takeshima <takeshimakai@gmail.com>
2022-03-08 07:50:35 -0500

    unauthorized style

--- src/pages/Unauthorized/index.js
+++ src/pages/Unauthorized/index.js
@@ -11,14 +11,17 @@ const Unauthorized = ({ location }) => {
   }, [history, location.state]);
 
   return (
-    <div>
-      <p>It appears you've requested to perform an unauthorized action.</p>
-      <p>Please sign in and try again.</p>
-      <Link to='/'>
-        <button className='border active:border-0 active:p-px shadow-md max-w-2xs w-full h-8 rounded-full text-gray-600 bg-gray-50 text-sm hover:bg-gray-200 active:shadow-inner'>
-          Proceed to sign in
-        </button>
-      </Link>
+    <div className='h-screen w-screen grid grid-rows-2 justify-items-center'>
+      <h2 className='self-center text-gray-800 font-serif text-6xl sm:text-8xl '>listen</h2>
+      <div className='text-center'>
+        <p className='font-light sm:text-sm'>It appears you've requested to perform an unauthorized action.</p>
+        <p className='font-light sm:text-sm'>Please sign in and try again.</p>
+        <Link to='/'>
+          <button className='mt-10 border active:border-0 active:p-px shadow-md max-w-2xs w-full h-8 rounded-full text-gray-600 bg-gray-50 text-sm hover:bg-gray-200 active:shadow-inner'>
+            Proceed to sign in
+          </button>
+        </Link>
+      </div>
     </div>
   )
 }
