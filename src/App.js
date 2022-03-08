919cd59dffe6b6c265b2d5b023336c210d54001b
Kai Takeshima <takeshimakai@gmail.com>
2022-03-08 10:37:55 -0500

    remove .js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import GoogleOAuthSuccess from './pages/GoogleOAuthSuccess';
 import Forum from './pages/Forum';
 import Profile from './pages/User/Profile';
 import Home from './pages/Home';
-import AccountSetUp from './pages/AccountSetUp.js';
+import AccountSetUp from './pages/AccountSetUp';
 import Main from './pages/Main';
 import EmailVerification from './pages/EmailVerification';
 import Friends from './pages/Friends';
