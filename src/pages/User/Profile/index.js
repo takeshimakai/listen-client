f0c911ff2309dac10d68e2a1a30efdb0095ec677
Kai Takeshima <takeshimakai@gmail.com>
2022-03-18 13:11:18 -0400

    profile height bug

--- src/pages/User/Profile/index.js
+++ src/pages/User/Profile/index.js
@@ -21,6 +21,7 @@ import PrimaryBtn from "../../../components/PrimaryBtn";
 import TertiaryBtn from "../../../components/TertiaryBtn";
 
 import useFriendshipStatus from '../../../hooks/useFriendshipStatus';
+import useWindowWidth from '../../../hooks/useWindowWidth';
 
 const Profile = () => {
   const history = useHistory();
@@ -28,6 +29,7 @@ const Profile = () => {
   const { token, setToken } = useContext(UserContext);
   const { id } = decodeToken(token);
 
+  const windowWidth = useWindowWidth();
   const { friendshipStatus, setFriendshipStatus } = useFriendshipStatus(userId);
   const [editMode, setEditMode] = useState(false);
   const [compose, setCompose] = useState(false);
@@ -82,46 +84,91 @@ const Profile = () => {
     })();
   }, [token, userId, id, history, setToken]);
 
+  useEffect(() => {
+    if (windowWidth >= 1280) {
+      const box = document.querySelector('#user-profile');
+      box.style.maxHeight = window.innerHeight * 0.8 + 'px';
+    }
+  }, [windowWidth]);
+
   return (
     <>
       {compose && <DMForm userId={userId} username={profile.username} setCompose={setCompose} />}
       {editMode
         ? <ProfileForm profile={profile} setProfile={setProfile} setEditMode={setEditMode} />
-        : <div className='h-screen pt-16 sm:pt-20 pb-4 sm:pb-10 px-4 xl:px-0 flex flex-col xl:flex-row items-center xl:items-start xl:relative xl:w-max xl:mx-auto'>
-            <div>
-              <img
-                className='h-36 w-36 sm:h-48 sm:w-48 rounded-full object-cover'
-                src={
-                  profile && profile.img
-                    ? `data:${profile.img.contentType};base64,${profile.img.data}`
-                    : defaultPic
-                }
-                alt=''
-              />
-            </div>
-            <div className='rounded-xl min-h-0 h-full w-full flex flex-col items-center xl:items-start xl:ml-20 xl:w-60'>
-              <p className='mb-4 mt-4 xl:mt-0 xl:mt-0 text-lg font-bold text-gray-800'>{profile && profile.username}</p>
-              <div className='scroll-fade no-scrollbar pt-5 pb-10 overflow-auto h-full w-full space-y-9'>
-                <Card title='Date of birth' data={profile && profile.dob} />
-                <Card title='Gender' data={profile && profile.gender} />
-                <Card title='Interests' data={profile && profile.interests} />
-                <Card title='Topics' data={profile && profile.problemTopics} />
+        : windowWidth >= 1280
+          ? <div className='flex-grow pt-20 grid grid-cols-2 gap-x-20'>
+              <div className='justify-self-end'>
+                <div>
+                  <img
+                    className='h-48 w-48 rounded-full object-cover'
+                    src={
+                      profile && profile.img
+                        ? `data:${profile.img.contentType};base64,${profile.img.data}`
+                        : defaultPic
+                    }
+                    alt=''
+                  />
+                </div>
+                <div className='flex flex-col items-center space-y-2.5 mt-6'>
+                  {userId
+                    ? <>
+                        <FriendBtns
+                          userId={userId}
+                          friendshipStatus={friendshipStatus}
+                          setFriendshipStatus={setFriendshipStatus}
+                        />
+                        {friendshipStatus === 'friends' && <PrimaryBtn label='Send message' onClick={() => setCompose(true)} />}
+                      </>
+                    : <TertiaryBtn label='Edit' onClick={() => setEditMode(true)} />
+                  }
+                </div>
+              </div>
+              <div>
+                <p className='text-lg font-bold text-gray-800 mb-4'>{profile && profile.username}</p>
+                <div id='user-profile' className='scroll-fade no-scrollbar pt-5 pb-10 overflow-auto space-y-9'>
+                  <Card title='Date of birth' data={profile && profile.dob} />
+                  <Card title='Gender' data={profile && profile.gender} />
+                  <Card title='Interests' data={profile && profile.interests} />
+                  <Card title='Topics' data={profile && profile.problemTopics} />
+                </div>
               </div>
             </div>
-            <div className='mt-6 xl:mt-0 xl:absolute xl:left-4 xl:top-72 w-full max-w-xs xl:w-40 flex justify-center xl:flex-col space-x-2 xl:space-x-0 xl:space-y-2.5'>
-              {userId
-                ? <>
-                    <FriendBtns
-                      userId={userId}
-                      friendshipStatus={friendshipStatus}
-                      setFriendshipStatus={setFriendshipStatus}
-                    />
-                    {friendshipStatus === 'friends' && <PrimaryBtn label='Send message' onClick={() => setCompose(true)} />}
-                  </>
-                : <TertiaryBtn label='Edit' onClick={() => setEditMode(true)} />
-              }
+          : <div className='flex-grow max-h-screen pt-16 sm:pt-20 pb-4 sm:pb-10 px-4 flex flex-col items-center'>
+              <div>
+                <img
+                  className='h-36 w-36 sm:h-48 sm:w-48 rounded-full object-cover'
+                  src={
+                    profile && profile.img
+                      ? `data:${profile.img.contentType};base64,${profile.img.data}`
+                      : defaultPic
+                  }
+                  alt=''
+                />
+              </div>
+              <div className='min-h-0 flex-grow w-full flex flex-col items-center'>
+                <p className='mb-4 mt-4 text-lg font-bold text-gray-800'>{profile && profile.username}</p>
+                <div className='scroll-fade no-scrollbar pt-5 pb-10 overflow-auto w-full space-y-9'>
+                  <Card title='Date of birth' data={profile && profile.dob} />
+                  <Card title='Gender' data={profile && profile.gender} />
+                  <Card title='Interests' data={profile && profile.interests} />
+                  <Card title='Topics' data={profile && profile.problemTopics} />
+                </div>
+              </div>
+              <div className='mt-6 w-full max-w-xs flex justify-center space-x-2'>
+                {userId
+                  ? <>
+                      <FriendBtns
+                        userId={userId}
+                        friendshipStatus={friendshipStatus}
+                        setFriendshipStatus={setFriendshipStatus}
+                      />
+                      {friendshipStatus === 'friends' && <PrimaryBtn label='Send message' onClick={() => setCompose(true)} />}
+                    </>
+                  : <TertiaryBtn label='Edit' onClick={() => setEditMode(true)} />
+                }
+              </div>
             </div>
-          </div>
       }
     </>
   )
